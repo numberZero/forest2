6c0928d3e503aebb514370824eb535475a4483be
numzero <numzer0@yandex.ru>
2021-02-18 04:00:25 +0300

    Поддержка нескольких моделей

--- model_to_texture.py
+++ model_to_texture.py
@@ -26,6 +26,8 @@ hsteps = np.array([max(1, int(h_circle_steps * math.sin(k * v_step))) for k in r
 hsteps_ends = np.cumsum(hsteps, dtype='uint32')
 view_count = hsteps_ends[-1]
 
+bill_threshold = 15.0
+
 #format, pixel_size = GL_R3_G3_B2, 1
 format, pixel_size = GL_RGB5_A1, 2
 #format, pixel_size = GL_RGBA8, 4
@@ -103,9 +105,9 @@ def link_program(*shaders):
 		raise Error('Shader compilation error: %s\n' % glGetProgramInfoLog(program))
 	return program
 
-def load_mesh(name, parts = 2):
+def load_mesh(name, colors):
 	mesh = []
-	for part in range(parts):
+	for part, color in enumerate(colors):
 		index_data = read_file(f'{name}.{part}.i')
 		vertex_data = read_file(f'{name}.{part}.v')
 		icount = len(index_data) // 2
@@ -116,10 +118,19 @@ def load_mesh(name, parts = 2):
 		glBindBuffer(GL_SHADER_STORAGE_BUFFER, vbuf)
 		glBufferStorage(GL_SHADER_STORAGE_BUFFER, len(vertex_data), vertex_data, 0)
 		glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0)
-		mesh.append((vbuf, ibuf, icount))
+		mesh.append((vbuf, ibuf, icount, color))
 	return mesh
 
-meshes = {}
+def make_hstb():
+	global hstb
+	hstb = glGenBuffers(1)
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, hstb)
+	glBufferStorage(GL_SHADER_STORAGE_BUFFER, 4 * len(hsteps_ends), hsteps_ends, 0)
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0)
+
+class Mesh:
+	pass
+meshes = []
 
 def init():
 	global program_mesh, program_bill, program_split
@@ -140,10 +151,23 @@ def init():
 	glEnable(GL_DEPTH_TEST)
 	glEnable(GL_LINE_SMOOTH)
 	glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
-	for kind in 'maple', 'pine', 'willow':
-		meshes[kind] = load_mesh(kind, 2)
-	prerender_bills()
-	prepare_instances()
+	br = BillRenderer()
+	br.enter(levels)
+	for prob, kind, colors in [
+			(0.3, 'maple', [(0.075554, 0.035952, 0.021588), (0.042570, 0.108587, 0.004716)]),
+			(0.2, 'pine', [(0.174414, 0.084845, 0.052037), (0.003064, 0.081508, 0.000000)]),
+			(0.5, 'willow', [(0.055724, 0.034397, 0.025506), (0.041258, 0.184065, 0.029954)]),
+			]:
+		mesh = load_mesh(kind, colors)
+		tree = Mesh()
+		tree.name = kind
+		tree.prob = prob
+		tree.bufs = mesh
+		tree.bill = br.render(mesh)
+		meshes.append(tree)
+	br.leave()
+	make_hstb()
+	prepare_instances([tree.prob for tree in meshes], 1.5, 100)
 	prepare_split()
 	glClearColor(0.0, 0.0, 0.2, 1.0)
 	glEnable(GL_MULTISAMPLE)
@@ -224,72 +248,48 @@ def render_tripod():
 colors = [(0.5, 0.5, 0.5), (0.3, 0.5, 0.0)]
 model_matrix = glm.scale(make_ortho_matrix(), vec3(0.5))
 
-def render_thing():
-	glUseProgram(program_mesh)
-	glUniformMatrix4fv(0, 1, GL_FALSE, projection_matrix * camera_matrix)
-	glUniformMatrix4fv(1, 1, GL_FALSE, model_matrix)
-
-	glEnableVertexAttribArray(0)
-	glEnableVertexAttribArray(3)
-	glVertexAttrib3f(2, 0.0, 0.0, 0.0)
-
-	for (vbuf, ibuf, count), color in zip(meshes['maple'], colors):
-		glVertexAttrib3f(1, *color)
-
-		glBindBuffer(GL_ARRAY_BUFFER, vbuf)
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
-		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(20))
-		glBindBuffer(GL_ARRAY_BUFFER, 0)
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuf)
-		glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_SHORT, ctypes.c_void_p(0))
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
-
-	glDisableVertexAttribArray(0)
-	glDisableVertexAttribArray(3)
-	glUseProgram(0)
-
-def prepare_instances(spacing = 2.0, gsize = 25):
-	global obuf, ocount
+def prepare_instances(probs, spacing = 2.0, gsize = 25):
+	global obuf, ocount, ocounts, rad
 	n = 2 * gsize + 1
-	x = spacing * gsize
-	verts = np.stack([
-		np.broadcast_to(np.linspace(-x, x, n), (n, n)),
-		np.broadcast_to(np.linspace(-x, x, n), (n, n)).transpose(),
-		np.zeros((n, n)),
-		np.ones((n, n)),
-		], axis=2).reshape((-1, 4))
+	rad = spacing * gsize
+	ocount = n * n
 	rng = rnd.default_rng()
-	rng.shuffle(verts)
-	ocount = len(verts)
-	verts = np.array(verts, dtype='float32')
+	xx = np.broadcast_to(np.linspace(-rad, rad, n), (n, n))
+	yy = xx.transpose()
+	zz = np.zeros((n, n))
+	ww = rng.choice(len(probs), p=probs, size=(n, n))
+	ocounts = np.bincount(ww.flat)
+	verts = np.array(np.stack([xx, yy, zz, ww], axis=2).reshape((-1, 4)), dtype='float32')
 	obuf = glGenBuffers(1)
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, obuf)
 	glBufferStorage(GL_SHADER_STORAGE_BUFFER, len(verts) * 16, verts, 0)
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0)
 
 def prepare_split():
-	global indbuf, nbuf, fbuf
-	indirects = np.array([
-		# near
-		0, # count
-		0, # instance count
-		0, # first index
-		0, # base vertex
-		0, # base instance
-		# far
-		0, # count
-		1, # instance count
-		0, # first
-		0, # base instance
-		], dtype='uint32')
+	global indirects, indbuf, nbuf, fbuf
+	parts = len(ocounts)
+	bases = np.roll(np.cumsum(ocounts), 1)
+	bases[0] = 0
+	indirects = np.array(np.stack([
+			# near
+			np.zeros(parts, dtype='uint32'), # count
+			np.zeros(parts, dtype='uint32'), # instance count
+			np.zeros(parts, dtype='uint32'), # first index
+			np.zeros(parts, dtype='uint32'), # base vertex
+			bases, # base instance
+			# far
+			np.zeros(parts, dtype='uint32'), # count
+			np.ones(parts, dtype='uint32'), # instance count
+			bases, # first
+			np.zeros(parts, dtype='uint32'), # base instance
+			], axis=1).reshape((parts, 9)).flat, dtype='uint32')
 	indbuf, nbuf, fbuf = glGenBuffers(3)
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, nbuf)
 	glBufferStorage(GL_SHADER_STORAGE_BUFFER, ocount * 16, None, 0)
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, fbuf)
 	glBufferStorage(GL_SHADER_STORAGE_BUFFER, ocount * 16, None, 0)
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, indbuf)
-	glBufferStorage(GL_SHADER_STORAGE_BUFFER, len(indirects) * 4, indirects, GL_DYNAMIC_STORAGE_BIT)
+	glBufferStorage(GL_SHADER_STORAGE_BUFFER, len(indirects) * 4, None, GL_DYNAMIC_STORAGE_BIT)
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0)
 
 def render():
@@ -301,19 +301,16 @@ def render():
 
 	glUseProgram(program_split)
 	glUniform3fv(0, 1, position)
-	#glUniform1f(1, 1, threshold)
+	glUniform1f(1, bill_threshold)
 	glBindBuffer(GL_DRAW_INDIRECT_BUFFER, indbuf)
-	glBufferSubData(GL_DRAW_INDIRECT_BUFFER, 4, ctypes.c_uint32(0))
-	glBufferSubData(GL_DRAW_INDIRECT_BUFFER, 20, ctypes.c_uint32(0))
+	glBufferSubData(GL_DRAW_INDIRECT_BUFFER, 0, indirects)
 	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, obuf)
-	glBindBufferRange(GL_ATOMIC_COUNTER_BUFFER, 1, indbuf, 0, 20)
 	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, nbuf)
-	glBindBufferRange(GL_ATOMIC_COUNTER_BUFFER, 2, indbuf, 20, 16)
 	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, fbuf)
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, indbuf)
 	glDispatchCompute(ocount, 1, 1)
 	glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT | GL_COMMAND_BARRIER_BIT)
-	n = ocount
-	m = n // 5
+	#print(np.frombuffer(glGetBufferSubData(GL_DRAW_INDIRECT_BUFFER, 0, 36 * 3), dtype='uint32').reshape((-1, 9)))
 
 	glUseProgram(program_mesh)
 	glUniformMatrix4fv(0, 1, GL_FALSE, projection_matrix * camera_matrix)
@@ -327,18 +324,19 @@ def render():
 	glBindBuffer(GL_ARRAY_BUFFER, nbuf)
 	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 16, ctypes.c_void_p(0))
 
-	for (vbuf, ibuf, count), color in zip(meshes['maple'], colors):
-		glVertexAttrib3f(1, *color)
+	for k in range(len(ocounts)):
+		for vbuf, ibuf, count, color in meshes[k].bufs:
+			glVertexAttrib3f(1, *color)
 
-		glBindBuffer(GL_ARRAY_BUFFER, vbuf)
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
-		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(20))
-		glBindBuffer(GL_ARRAY_BUFFER, 0)
+			glBindBuffer(GL_ARRAY_BUFFER, vbuf)
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
+			glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(20))
+			glBindBuffer(GL_ARRAY_BUFFER, 0)
 
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuf)
-		glBufferSubData(GL_DRAW_INDIRECT_BUFFER, 0, ctypes.c_uint32(count))
-		glDrawElementsIndirect(GL_TRIANGLES, GL_UNSIGNED_SHORT, ctypes.c_void_p(0))
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuf)
+			glBufferSubData(GL_DRAW_INDIRECT_BUFFER, 36 * k, ctypes.c_uint32(count))
+			glDrawElementsIndirect(GL_TRIANGLES, GL_UNSIGNED_SHORT, ctypes.c_void_p(36 * k))
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
 
 	glVertexAttribDivisor(2, 0)
 	glDisableVertexAttribArray(0)
@@ -348,10 +346,10 @@ def render():
 	glUseProgram(0)
 	glColor3f(0.0, 0.1, 0.0)
 	glBegin(GL_QUADS)
-	glVertex2f(-1000.0, -1000.0)
-	glVertex2f(-1000.0,  1000.0)
-	glVertex2f( 1000.0,  1000.0)
-	glVertex2f( 1000.0, -1000.0)
+	glVertex2f(-rad, -rad)
+	glVertex2f(-rad,  rad)
+	glVertex2f( rad,  rad)
+	glVertex2f( rad, -rad)
 	glEnd()
 
 	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
@@ -359,8 +357,6 @@ def render():
 	glUniformMatrix4fv(0, 1, GL_FALSE, projection_matrix)
 	glUniformMatrix3fv(1, 1, GL_FALSE, mat3(camera_matrix))
 	glUniform3fv(2, 1, position)
-	glBindTextureUnit(0, 1)
-	glBindTextureUnit(1, 2)
 	glVertexAttrib2f(1, 1.0, 1.0)
 	glUniform1i(3, v_halfcircle_steps)
 	glEnableVertexAttribArray(0)
@@ -368,83 +364,103 @@ def render():
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 16, ctypes.c_void_p(0))
 	glBindBuffer(GL_ARRAY_BUFFER, 0)
 	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, hstb)
-	glDrawArraysIndirect(GL_POINTS, ctypes.c_void_p(20))
+	for k in range(len(ocounts)):
+		glBindTextureUnit(0, meshes[k].bill)
+		glDrawArraysIndirect(GL_POINTS, ctypes.c_void_p(36 * k + 20))
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0)
 	glDisableVertexAttribArray(0)
 	glUseProgram(0)
 	glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
 	glBindBuffer(GL_DRAW_INDIRECT_BUFFER, 0)
 
-def prerender_bills():
-	global projection_matrix, camera_matrix
+class BillRenderer:
+	def enter(self, mip_levels):
+		self.levels = mip_levels
+		self.size = 1 << mip_levels
 
-	glClearColor(0.0, 0.0, 0.0, 0.0) #обязательно α=0
+		self.program = program_mesh
 
-	projection_matrix = make_ortho_matrix()
+		self.projection_matrix = make_ortho_matrix()
+		self.model_matrix = model_matrix
 
-	size = 1 << levels
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 1)
+		self.depth = glGenTextures(1)
+		glBindTexture(GL_TEXTURE_2D, self.depth)
+		glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT16, self.size, self.size)
 
-	glBindTexture(GL_TEXTURE_2D_ARRAY, 1)
-	#glTexStorage3D(GL_TEXTURE_2D_ARRAY, 1, format, size, size, view_count)
-	glTexStorage3D(GL_TEXTURE_2D_ARRAY, levels, format, size, size, view_count)
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-	glTexParameterf(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAX_ANISOTROPY, 8.0)
-	glTexParameterf(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_LOD_BIAS, -0.5)
+		self.fb = glGenFramebuffers(1)
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, self.fb)
+		glFramebufferTexture(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, self.depth, 0)
 
-	glBindTexture(GL_TEXTURE_2D_ARRAY, 2)
-	glTexStorage3D(GL_TEXTURE_2D_ARRAY, 1, GL_DEPTH_COMPONENT16, size, size, view_count)
-	#glTexStorage3D(GL_TEXTURE_2D_ARRAY, levels, GL_DEPTH_COMPONENT16, size, size, view_count)
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-	glTexParameterf(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAX_ANISOTROPY, 8.0)
+	def leave(self):
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0)
+		glDeleteFramebuffers(1, [self.fb])
+		glDeleteTextures([self.depth])
 
-	glVertexAttrib3f(2, 0, 0, 0)
-	glViewport(0, 0, size, size)
-	view = 0
-	for v in range(vsteps):
-		va = v * v_step
-		h_step_count = hsteps[v]
-		h_step = 2 * math.pi / h_step_count
-		for u in range(h_step_count):
-			ha = u * h_step
-			camera_matrix = glm.translate(
-				glm.rotate(
+	def render(self, mesh):
+		layers = glGenTextures(1)
+		glBindTexture(GL_TEXTURE_2D_ARRAY, layers)
+		#glTexStorage3D(GL_TEXTURE_2D_ARRAY, 1, format, size, size, view_count)
+		glTexStorage3D(GL_TEXTURE_2D_ARRAY, self.levels, format, self.size, self.size, view_count)
+		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+		glTexParameterf(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAX_ANISOTROPY, 8.0)
+		glTexParameterf(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_LOD_BIAS, -0.5)
+
+		glUseProgram(self.program)
+		glUniformMatrix4fv(1, 1, GL_FALSE, self.model_matrix)
+		glEnableVertexAttribArray(0)
+		glEnableVertexAttribArray(3)
+		glVertexAttrib3f(2, 0.0, 0.0, 0.0)
+		glViewport(0, 0, self.size, self.size)
+		view = 0
+		for v in range(vsteps):
+			va = v * v_step
+			h_step_count = hsteps[v]
+			h_step = 2 * math.pi / h_step_count
+			for u in range(h_step_count):
+				ha = u * h_step
+				camera_matrix = glm.translate(
 					glm.rotate(
-						mat4(1.0),
-						#float(0.0),
-						0.5 * math.pi - va,
-						vec3(1.0, 0.0, 0.0)),
-					ha,
-					vec3(0.0, 0.0, 1.0)),
-				vec3(0.0, 0.0, -0.5))
-			glFramebufferTextureLayer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 1, 0, view)
-			glFramebufferTextureLayer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, 2, 0, view)
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-			render_thing()
-			view += 1
-	assert(view == view_count)
+						glm.rotate(
+							mat4(1.0),
+							#float(0.0),
+							0.5 * math.pi - va,
+							vec3(1.0, 0.0, 0.0)),
+						ha,
+						vec3(0.0, 0.0, 1.0)),
+					vec3(0.0, 0.0, -0.5))
+				glFramebufferTextureLayer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, layers, 0, view)
+				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+				glUniformMatrix4fv(0, 1, GL_FALSE, self.projection_matrix * camera_matrix)
 
-	image_size = size**2
-	total_layer_size = pixel_size * image_size / 0.75 # 0.75 for mipmaps
-	total_gram = view_count * total_layer_size
-	print(f'{total_gram / 1024**2} MiB GPU RAM used for the layers')
+				for vbuf, ibuf, count, color in mesh:
+					glVertexAttrib3f(1, *color)
 
-	global hstb
-	hstb = glGenBuffers(1)
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, hstb)
-	glBufferStorage(GL_SHADER_STORAGE_BUFFER, 4 * len(hsteps_ends), hsteps_ends, 0)
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0)
+					glBindBuffer(GL_ARRAY_BUFFER, vbuf)
+					glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
+					glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(20))
+					glBindBuffer(GL_ARRAY_BUFFER, 0)
 
-	glInvalidateFramebuffer(GL_DRAW_FRAMEBUFFER, 1, [GL_DEPTH_ATTACHMENT])
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0)
-	glGenerateTextureMipmap(1)
-	glGenerateTextureMipmap(2)
+					glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuf)
+					glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_SHORT, ctypes.c_void_p(0))
+					glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
+
+				view += 1
+		assert(view == view_count)
+		glDisableVertexAttribArray(0)
+		glDisableVertexAttribArray(3)
+		glUseProgram(0)
+
+		glGenerateTextureMipmap(layers)
+
+		image_size = self.size**2
+		total_layer_size = pixel_size * image_size / 0.75 # 0.75 for mipmaps
+		total_gram = view_count * total_layer_size
+		print(f'{total_gram / 1024**2} MiB GPU RAM used for the layers')
+
+		return layers
 
 def resize_window(wnd, width: int, height: int):
 	global projection_matrix
--- split.c.glsl
+++ split.c.glsl
@@ -1,5 +1,25 @@
 #version 450
 
+layout(std430) struct DrawElementsIndirectCommand {
+	uint count;
+	uint instanceCount;
+	uint firstIndex;
+	uint baseVertex;
+	uint baseInstance;
+};
+
+layout(std430) struct DrawArraysIndirectCommand {
+	uint count;
+	uint instanceCount;
+	uint first;
+	uint baseInstance;
+};
+
+layout(std430) struct Indirect {
+	DrawElementsIndirectCommand near;
+	DrawArraysIndirectCommand far;
+};
+
 layout(local_size_x = 1) in;
 
 layout(location = 0) uniform vec3 camera_position;
@@ -9,24 +29,28 @@ layout(binding = 0) readonly restrict buffer SourcePositions {
 	vec4 position[];
 };
 
-layout(binding = 1) uniform atomic_counter near_objects;
 layout(binding = 1) writeonly restrict buffer NearPositions {
 	vec4 npos[];
 };
 
-layout(binding = 2) uniform atomic_counter far_objects;
 layout(binding = 2) writeonly restrict buffer FarPositions {
 	vec4 fpos[];
 };
 
+layout(binding = 3) restrict buffer Indirects {
+	Indirect inds[];
+};
+
 void main() {
-	vec4 pos = position[gl_GlobalInvocationID.x];
+	vec4 pk = position[gl_GlobalInvocationID.x];
+	vec4 pos = vec4(pk.xyz, 1.0);
+	uint kind = uint(pk.w);
 	float dist = distance(camera_position, pos.xyz);
 	if (dist < threshold) {
-		uint index = atomicCounterIncrement(near_objects);
-		npos[index] = pos;
+		uint index = atomicAdd(inds[kind].near.instanceCount, 1);
+		npos[inds[kind].near.baseInstance + index] = pos;
 	} else {
-		uint index = atomicCounterIncrement(far_objects);
-		fpos[index] = pos;
+		uint index = atomicAdd(inds[kind].far.count, 1);
+		fpos[inds[kind].far.first + index] = pos;
 	}
 }
